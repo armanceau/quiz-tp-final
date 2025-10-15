7ac40a959770dc287f079efef7c3e0f1c1cc3510
Arthur Manceau <102350268+armanceau@users.noreply.github.com>
2025-10-15 10:43:54 +0200

    feat(): Ajout d'un quiz par catégorie

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -1,27 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from "@angular/router";
-import { QuizService } from "../shared/services/quiz.service";
+import { ActivatedRoute, Router } from '@angular/router';
+import { QuizService } from '../shared/services/quiz.service';
 
 @Component({
   selector: 'app-quiz',
   templateUrl: './quiz.component.html',
   styleUrls: ['./quiz.component.scss'],
-  standalone: false
+  standalone: false,
 })
 export class QuizComponent implements OnInit {
   isQuizFinished = this.quizService.isQuizFinished;
   playerName = '';
+  categorieID = 0;
 
   constructor(
     private quizService: QuizService,
     private router: Router,
     private route: ActivatedRoute
-  ) { }
+  ) {}
 
   ngOnInit(): void {
-    this.route.params.subscribe(params => {
+    this.route.params.subscribe((params) => {
       this.quizService.playerName = params['playerName'];
       this.playerName = params['playerName'];
+      this.quizService.categorieID = params['categorieID'];
     });
   }
 
